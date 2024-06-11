c944c5c376f40864343511195d843608f7029eb8
Wes-Wei <jing.wei@mail.utoronto.ca>
2024-06-11 13:00:45 +0800

    Update Transparent Proxy

--- remotecurl/client/main.py
+++ remotecurl/client/main.py
@@ -4,11 +4,11 @@ from remotecurl.common.config import Conf
 
 __CONFIG__ = Conf()
 
+
 def request(flow: http.HTTPFlow) -> None:
     """Redirect all request to the server"""
     # Redirect Request
     original_url = flow.request.pretty_url
     new_url = __CONFIG__.client.redirect + original_url
     print(f"Redirect {original_url} to {new_url}")
-
-    
+    flow.request.url = new_url
