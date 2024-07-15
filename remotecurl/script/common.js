9c81c36fdb5b08c567ee253f0f6087efb4d10dea
Wes-KW <dotdotdashdash2024@hotmail.com>
2024-07-15 06:20:03 +0000

    Minor Patch

--- remotecurl/script/common.js
+++ remotecurl/script/common.js
@@ -24,29 +24,12 @@ function get_requested_url(relative_url) {
 	if (relative_url === "#") {
 		return relative_url;
 	} else {
-		let abs_url = new URL(relative_url, $url).href;
-		if (check_url(abs_url)) {
-			return $path + abs_url;
+		if (relative_url === $path + $url) {
+			return relative_url;
 		} else {
-			let check_list = [$path];
-			for (let i = 0; i < check_list.length; i++){
-				const url = check_list[i];
-				if (relative_url.startsWith(url)) {
-					try{
-						let new_m_url = relative_url.substring(url.length);
-						let url_obj = new URL($url);
-						let new_m_url_obj = new URL(new_m_url, url_obj.origin);
-						if (check_url(new_m_url_obj.href)) {
-							return $path + new_m_url_obj.href;
-						}
-					} catch (e) {
-						continue;
-					}
-
-				}
-			}
+			let abs_url = new URL(relative_url, $url).href
+			return $path + abs_url;
 		}
-		return relative_url;
 	}
 }
 
