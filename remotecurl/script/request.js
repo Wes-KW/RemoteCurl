c3aa1c132f6a2b1c4ba14c39d18b0edec6cdd0c1
Wes-KW <dotdotdashdash2024@hotmail.com>
2024-07-17 06:22:34 +0000

    Minor Patch

--- remotecurl/script/request.js
+++ remotecurl/script/request.js
@@ -7,17 +7,6 @@ window.XMLHttpRequest.prototype.open = function(method, url, async=true) {
     this._open(method, req_url, async);
 }
 
-window.Request = new Proxy(
-	window.Request, {
-		construct(target, args) {
-			let req_url = get_requested_url(args[0])
-			redirect_log("Request", args[0], req_url);
-			args[0] = req_url;
-			return new target(...args);
-		}
-	}
-);
-
 window._fetch = window.fetch;
 window.fetch = function(url, options) {
 	let req_url = url;
@@ -38,4 +27,30 @@ window.Navigator.prototype.sendBeacon = function(url, data=null) {
 	let req_url = get_requested_url(url);
 	redirect_log("navigator.sendBeacon", url, req_url);
 	this._sendBeacon(req_url, data);
-};
+}
+
+window.ServiceWorkerContainer.prototype._register = window.ServiceWorkerContainer.prototype.register;
+window.ServiceWorkerContainer.prototype.register = function(scriptURL, options) {
+	let req_url = get_requested_url(scriptURL);
+	redirect_log("ServiceWorkerContainer.register", scriptURL, req_url);
+
+	let opt_scope = "/";
+	if (typeof options.scope != "undefined") {
+		opt_scope = options.scope;
+	}
+	let req_opt_scope = get_requested_url(opt_scope);
+	redirect_log("ServiceWorkerContainer.register.options.scope", opt_scope, req_opt_scope);
+
+	this._register(req_url, options);
+}
+
+window.Request = new Proxy(
+	window.Request, {
+		construct(target, args) {
+			let req_url = get_requested_url(args[0])
+			redirect_log("Request", args[0], req_url);
+			args[0] = req_url;
+			return new target(...args);
+		}
+	}
+);
