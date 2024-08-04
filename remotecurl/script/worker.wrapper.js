6683ad0f37bc17a7499e80be85b531d3ac9d9ee6
Wes-KW <dotdotdashdash2024@hotmail.com>
2024-08-04 01:20:15 +0000

    Update script

--- remotecurl/script/worker.wrapper.js
+++ remotecurl/script/worker.wrapper.js
@@ -1,9 +1,9 @@
 // worker.wrapper.js
 
-const create_proxied_worker = function(worker) {
+const create_proxied_worker = function(_worker) {
     const extra_overwrite = [
         "self", "location", "importScripts"
     ];
 
-    const worker = create_proxied_web_object(worker, extra_overwrite);
+    const worker = create_proxied_web_object(_worker, extra_overwrite);
 }
